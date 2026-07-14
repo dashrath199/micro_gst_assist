209648f1ddeb412cfd337b68fed620d59061843c
Dashrath R <dashrath199@gmail.com>
2026-07-14 15:18:20 +0530

    Cleanup: Remove dead code from setup.py reading requirements.txt
    install_requires is now hardcoded to [] since frappe/erpnext are provided by the bench.

--- setup.py
+++ setup.py
@@ -1,8 +1,5 @@
 from setuptools import setup, find_packages
 
-with open("requirements.txt") as f:
-    install_requires = f.read().strip().split("\n")
-
 setup(
     name="micro_gst_assist",
     version="0.0.1",
